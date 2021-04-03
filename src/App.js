5011e5d3178043ef1dfdfbe8fbda0fe7f391787e
hillary26121 <hillaryywallace@gmail.com>
2021-04-03 09:53:32 -0400

    got cards to add, added like feature

--- src/App.js
+++ src/App.js
@@ -1,35 +1,140 @@
-import './App.css';
-import MediaCards from './components/media-cards'
-import ListCards from './components/list-cards'
-import {useState} from 'react';
-
+import "./App.css";
+import ListCards from "./components/list-cards";
+import { useState } from "react";
 
 function App() {
-  const [listCards, setListCards] = useState([{
-    item : 'go grocery shopping'
-  },
-  {
-    item: 'pooped'
-  }])
 
-  const newListCards = (index, title) =>{
-    const renderedListCards = [...listCards];
-    renderedListCards[index] = {
-      title: title
-    }
+ 
+
+  const [listCards, setListCards] = useState([
+    {userInput: "hello", likes: 0, dislikes: 0, category: "went-well"},
+  ]);
+  const [likes, setLikes] = useState(0);
+
+  const addListCard = (e) => {
+    setListCards([
+      ...listCards,
+      {
+        userInput: "",
+        likes: 0,
+        dilikes: 0,
+        category: e.target.attributes.category.value,
+      },
+    ]);
+   
+  };
+  
+  function deleteItem() {
+  /*  setListCards(
+      listCards.filter((currItem, currIndex) => currIndex !== index)
+    );*/
   }
-  // const [wentWell, setWentWell] = useState({});
-  // const [toImprove, setToImprove] = useState({});
-  // const [actionItems, setActionItems] = useState({});
+
+  
+
+
+
+ 
   return (
-    <div id = 'layout'>
+    <main class="content row">
       <h1>Retro Board</h1>
-      <MediaCards>
-      <ListCards/>
-      </MediaCards>
-    </div>
-    
-    
+      <div>
+        Layout &nbsp;<button class="button button-default">Column</button>
+      </div>
+
+      <div class="RetroApp row">
+        <div class="RetroCategory RetroCategory-1">
+          
+          <h2>Went Well</h2>
+         
+          <button
+            onClick={addListCard}
+            category={"went-well"}
+            type="button"
+            class="ButtonAdd button button-default"
+            aria-label="Add new card"
+            title="Add new card"
+          >
+            +
+          </button>
+          {listCards
+            .filter((listCard) => listCard.category === 'went-well')
+
+            .map((listCard, index) => {
+              return <ListCards
+                userInput={listCard.userInput}
+                likes={listCard.likes}
+                dislikes={listCard.dislikes}
+                addListCard={addListCard}
+                deleteItem={deleteItem}
+                setLikes = {setLikes}
+                likes = {likes}
+                
+               
+        
+              />;
+            })}
+            
+        </div>
+        
+
+        <div class="RetroCategory RetroCategory-2">
+          <h2>To Improve</h2>
+          <button
+            onClick={addListCard}
+            category={"to-improve"}
+            type="button"
+            class="ButtonAdd button button-default"
+            aria-label="Add to new card"
+            title="Add to new card"
+          >
+            +
+          </button>
+          {listCards
+            .filter((listCard) => listCard.category === 'to-improve')
+
+            .map((listCard) => {
+              return <ListCards
+                userInput={listCard.userInput}
+                likes={listCard.likes}
+                dislikes={listCard.dislikes}
+                addListCard={addListCard}
+                deleteItem = {deleteItem}
+                setLikes = {setLikes}
+                likes = {likes}
+              />;
+            })}
+        </div>
+
+        <div class="RetroCategory RetroCategory-3">
+          <h2>Action Items</h2>
+          <button
+            onClick={addListCard}
+            category={"action-items"}
+            type="button"
+            class="ButtonAdd button button-default"
+            aria-label="Add to new card"
+            title="Add to new card"
+          >
+            +
+          </button>
+          {listCards
+            .filter((listCard) => listCard.category === 'action-items')
+
+            .map((listCard) => {
+              return <ListCards
+                userInput={listCard.userInput}
+                likes={listCard.likes}
+                dislikes={listCard.dislikes}
+                addListCard={addListCard}
+                deleteItem = {deleteItem}
+                setLikes = {setLikes}
+                likes = {likes}
+              />;
+            })}
+        </div>
+      </div>
+    </main>
   );
 }
 
--- src/components/list-cards.jsx
+++ src/components/list-cards.jsx
@@ -1,12 +1,54 @@
-import PropTypes from 'prop-types';
+import React from "react";
+import { useState } from 'react';
 
-function ListCards(props){
-    console.log(props)
-    return(
-     <div>
-         <input type="text" placeholder = "Item" aria-label = "Item" value = {props.title}/>
-     </div>
-    )
+function ListCards(props) {
+    const {listCards, likes, setLikes} = props;
+    
+    
+    
+ return (
+
+    <div>
+      <div class="RetroCard" aria-label="Retro card">
+        <textarea
+          class="textbox"
+          placeholder="Enter text here"
+          aria-label="Enter text here"
+          rows="1"
+          value={props.userInput}
+        ></textarea>
+
+        <div class="button-group">
+          <button type="button" class="button button-left" title="Move left">
+            <img src="angleLeft.svg" alt="Move left" width="12" height="12" />
+          </button>
+          <button type="button" class="button button-delete" title="Delete" onClick = {props.deleteItem()} >
+            Delete
+          </button>
+          <div>
+            <button type="button" class="button button-left" title="Like" onClick = {()=>setLikes(likes+1)}>
+              Like {likes}
+            </button>
+            <button
+             
+              value={props.likes}
+              type="button"
+              class="button button-left"
+              title="Dislike"
+            >
+              Dislike
+            </button>
+            <button
+              value={props.dislikes}
+              type="button"
+              class="button button-right"
+              title="Move right"
+            >Move right</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ListCards;
